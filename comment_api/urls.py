88c4db960c6c2600085ee4e3ab2ee97d95f09485
Pavel Kozhushko <81619884+pakodev28@users.noreply.github.com>
2022-04-20 08:01:00 +0300

    finally commit

--- comment_api/urls.py
+++ comment_api/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
         CreateSubCommentAPIView.as_view(),
     ),
     path(
-        "v1/subcomments/<int:pk>/", GetSubCommentAPIView.as_view(), name="subcomments"
+        "v1/comments/<int:pk>/", GetSubCommentAPIView.as_view(), name="subcomments"
     ),
 ]
